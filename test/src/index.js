26a034f0c0c016e932e84946c4040c9215b9e4c2
Irwin Lourtet <irwin.lourtet@egreen.fr>
2016-10-16 13:28:18 +0200

    [Test] Add test for add route with name

--- test/src/index.js
+++ test/src/index.js
@@ -13,7 +13,7 @@ let test = require('unit.js');
 let router = require('../../src');
 
 describe('Router ', function() {
-  it('should add route', function() {
+  it('should add route without name', function() {
     let route;
     let fn = function(){};
     let path = '/my-path';
@@ -26,6 +26,25 @@ describe('Router ', function() {
     route = router._routes[method + '_' + path];
     test.string(route.method).is(method);
     test.string(route.path).is(path);
+    test.string(route.name).is(method + '_' + path);
     test.function(route.fn).is(fn);
   });
+
+  it('should add route with name', function() {
+    let route;
+    let fn = function(){};
+    let path = '/my-path2';
+    let method = 'POST';
+    let name = 'myRouteName';
+    router.use({method, path, fn, name});
+
+    test.number(Object.keys(router._routes).length).is(2);
+    test.bool(router._routes.hasOwnProperty(method + '_' + path)).isTrue();
+
+    route = router._routes[method + '_' + path];
+    test.string(route.method).is(method);
+    test.string(route.path).is(path);
+    test.string(route.name).is(name);
+    test.function(route.fn).is(fn);
+  })
 });
